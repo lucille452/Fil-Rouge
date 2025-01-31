6d886d95d041786aa97da74a7628033560958c4c
pmaxence <maxence.patouillere@ynov.com>
2025-01-31 11:02:24 +0100

    feat(vm): test

--- main.tf
+++ main.tf
@@ -28,3 +28,26 @@ provider "proxmox" {
   pm_api_token_id = var.proxmox_api_token_id
   pm_api_token_secret = var.proxmox_api_token_secret
 }
+
+resource "proxmox_vm_qemu" "resource-name" {
+  name        = "VM-name"
+  target_node = "PVE-Max"
+
+  disks {
+    ide {
+      ide2 {
+        cdrom {
+          iso = "ISO file"
+        }
+      }
+    }
+  }
+
+  ### or for a Clone VM operation
+  # clone = "template to clone"
+
+  ### or for a PXE boot VM operation
+  # pxe = true
+  # boot = "scsi0;net0"
+  # agent = 0
+}
