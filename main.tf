caf67cb222fcfccf5105d7ad2f2ff93044f7a4df
pmaxence <maxence.patouillere@ynov.com>
2025-06-07 19:31:27 +0200

    fix (main.tf) : - c'est la bonne

--- main.tf
+++ main.tf
@@ -1,52 +1,43 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "Telmate/proxmox"
-      version = ">= 2.9.14"
-    }
-  }
-}
-
-provider "proxmox" {
-  pm_api_url          = var.proxmox_api_url
-  pm_api_token_id     = var.proxmox_api_token_id
-  pm_api_token_secret = var.proxmox_api_token_secret
-  pm_tls_insecure     = true
-}
-
 resource "proxmox_vm_qemu" "proxy_lb" {
-  name        = "Proxy"
-  target_node = "PVE-Max"
+  name        = "Proxy-test"
   clone       = "PROXY-LB"
-  full_clone  = true
-  os_type     = "cloud-init"
+  target_node = "PVE-Max"
+  vmid = 120
 
   cores   = 2
   sockets = 1
   memory  = 2048
   cpu     = "host"
 
-  scsihw   = "virtio-scsi-pci"
-  boot     = "cdn"
-  bootdisk = "scsi0"
+  os_type     = "cloud-init"
+  qemu_os = "l26"
   agent    = 1
+  scsihw   = "virtio-scsi-pci"
+  onboot = true
+  ciuser       = "proxy-usr"
+  cipassword   = var.cloudinit_password
+  ipconfig0    = "ip=192.168.10.100/24,gw=192.168.10.1"
+  nameserver   = "8.8.8.8"
 
-  disk {
-    slot     = 0
-    type     = "scsi"
-    storage  = "local"
-    size     = "10G"
-    format   = "qcow2"
-    iothread = 1
+  disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = "local"
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          storage    = "local"
+          size       = "10G"
+        }
+      }
+    }
   }
-
   network {
     model  = "virtio"
     bridge = "vmbr1"
   }
-
-  ipconfig0    = "ip=192.168.10.100/24,gw=192.168.10.1"
-  nameserver   = "8.8.8.8"
-  ciuser       = "ubuntu"
-  cipassword   = var.cloudinit_password
 }
